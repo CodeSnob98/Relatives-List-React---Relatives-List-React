451588e6ca68c6edccc7ef005e820a4df5b17c13
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-10-31 22:06:43 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,13 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import List from "./list";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.list = [
-      { key: "relativeListItem1", name: " Supratim" },
-      { key: "relativeListItem2", name: "Indra" },
-      { key: "relativeListItem3", name: "Ayan" },
-      { key: "relativeListItem4", name: "Dipayan" },
-      { key: "relativeListItem5", name: "Santanu" },
-      { key: "relativeListItem6", name: "Souradip" }
-    ];
-  }
   render() {
+    let list = [" Supratim", "Indra", "Ayan", "Dipayan", "Santanu", "Souradip"];
     return (
       <>
-        <ol>
-          {this.list.map((ele) => (
-            <li>
-              <List key={ele.key} name={ele.name} />
-            </li>
+        <ol kye="relativeList">
+          {list.map((ele, index) => (
+            <li key={`relativeListItem${index + 1}`}>{ele}</li>
           ))}
         </ol>
       </>
